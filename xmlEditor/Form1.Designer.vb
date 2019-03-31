e85b838120cf05907b5e5ea985009608f6e532df
Doofitator <ashley@sharkey.id.au>
2019-03-31 12:45:45 +1100

    Add project files.

---
+++ xmlEditor/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dgv_data = New System.Windows.Forms.DataGridView()
+        Me.btn_save = New System.Windows.Forms.Button()
+        CType(Me.dgv_data, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgv_data
+        '
+        Me.dgv_data.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv_data.Location = New System.Drawing.Point(12, 12)
+        Me.dgv_data.Name = "dgv_data"
+        Me.dgv_data.Size = New System.Drawing.Size(776, 405)
+        Me.dgv_data.TabIndex = 0
+        '
+        'btn_save
+        '
+        Me.btn_save.Location = New System.Drawing.Point(713, 423)
+        Me.btn_save.Name = "btn_save"
+        Me.btn_save.Size = New System.Drawing.Size(75, 23)
+        Me.btn_save.TabIndex = 1
+        Me.btn_save.Text = "Save"
+        Me.btn_save.UseVisualStyleBackColor = True
+        '
+        'frm_main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 451)
+        Me.Controls.Add(Me.btn_save)
+        Me.Controls.Add(Me.dgv_data)
+        Me.Name = "frm_main"
+        Me.Text = "XML Editor"
+        CType(Me.dgv_data, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents dgv_data As DataGridView
+    Friend WithEvents btn_save As Button
+End Class
---
+++ xmlEditor/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class frm_main
+    Dim ds As New DataSet
+    Dim fileName As String
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim ofd As New OpenFileDialog
+        ofd.Filter = "XML Files|*.xml"
+
+        If ofd.ShowDialog = DialogResult.OK Then
+            fileName = ofd.FileName
+            ds.ReadXml(fileName)
+            dgv_data.DataSource = ds.Tables(0)
+        Else
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub btn_save_Click(sender As Object, e As EventArgs) Handles btn_save.Click
+        ds.WriteXml(fileName)
+    End Sub
+
+    Private Sub frm_main_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        dgv_data.Width = Me.Width - 40
+        dgv_data.Height = Me.Height - 84
+        btn_save.Left = Me.Width - btn_save.Width - 27
+        btn_save.Top = Me.Height - btn_save.Height - 43
+    End Sub
+End Class
